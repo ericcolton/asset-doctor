839d33ff8a55becc8ccecc953015a4cb4fdc03c3
Eric Colton <sderic76@gmail.com>
2020-08-25 11:57:34 -0400

    improved output formating

--- rebalancer.py
+++ rebalancer.py
@@ -2,23 +2,30 @@
 
 import sys
 import re
+import math
+from enum import Enum
 from collections import namedtuple, defaultdict
 
-VALUE_TOLERANCE = 1
+
+class RoundingBehavior(Enum):
+    UP = 'UP'
+    DOWN = 'DOWN'
+    NEAREST = 'NEAREST'
+
+VALUE_TOLERANCE = 10
 PERCENT_TOLERANCE = 0.01
 
-SummaryRecord = namedtuple('Record', ['asset_class', 'ticker', 'target_percentage', 'balanced_amount', 'actual_amount'])
+SummaryRecord = namedtuple('Record', ['ticker', 'target_percentage', 'balanced_amount', 'actual_amount'])
 ImplementationRecord = namedtuple('ImplementationRecord', ['ticker', 'price', 'quantity'])
+Position = namedtuple('Position', ['ticker', 'quantity'])
+ModelPosition = namedtuple('ModelPosition', ['ticker', 'target_percentage'])
+RebalanceOptions = namedtuple('RebalanceOptions', ['desired_total_value', 'allow_fractional_shares', 'rounding_behavior'])
+RebalanceInstruction = namedtuple('RebalanceInstruction', ['ticker','op','quantity_str', 'shares_str', 'sign', 'value_str'])
 
 class NoImplmentationRecordException(Exception):
     pass
 class UnusedImplmentationRecordException(Exception):
     pass
-
-Position = namedtuple('Position', ['ticker', 'quantity'])
-ModelPosition = namedtuple('ModelPosition', ['ticker', 'target_percentage'])
-
-PERCENT_TOLERANCE = 0.01
 
 class PriceLookup:
     def __init__(self):
@@ -57,6 +64,9 @@ class Portfolio:
     def all_tickers(self):
         return self.positions.keys()
 
+    def calc_total_value(self, prices: PriceLookup):
+        return sum([prices.get_price(p.ticker) * p.quantity for p in self.positions.values()])
+
 class ModelPortfolioBuilder:
     def __init__(self, target_value: float, prices: PriceLookup):
         self.prices = prices
@@ -83,10 +93,10 @@ class ModelPortfolioBuilder:
 
 class PortfolioRebalancer:
 
-    def __init__(self, allow_fractional_shares: bool = False):
+    def __init__(self, options: RebalanceOptions):
+        self.options = options
         self.live_portfolio = None
         self.model_portfolio = None
-        self.allow_fractional_shares = allow_fractional_shares
 
     def set_live_portfolio(self, live_portfolio: Portfolio):
         self.live_portfolio = live_portfolio
@@ -94,7 +104,7 @@ class PortfolioRebalancer:
     def set_model_portfolio(self, model_portfolio: Portfolio):
         self.model_portfolio = model_portfolio
 
-    def build_rebalance_instructions(self):
+    def build_rebalance_instructions(self) -> dict:
         if not (self.live_portfolio and self.model_portfolio):
             raise Exception("live_portfolio and model_portfolio must both be set")
 
@@ -102,15 +112,36 @@ class PortfolioRebalancer:
         for mp_ticker in self.model_portfolio.all_tickers():
             mp_quantity = self.model_portfolio.get_quantity(mp_ticker)
             live_quantity = self.live_portfolio.get_quantity(mp_ticker)
-            ticker_delta = live_quantity - mp_quantity
-            if not allow_fractional_shares:
-                ticker_delta = round(ticker_delta)
+            ticker_delta = mp_quantity - live_quantity
+            if not options.allow_fractional_shares:
+                if options.rounding_behavior == RoundingBehavior.NEAREST:
+                    ticker_delta = round(ticker_delta)
+                elif options.rounding_behavior == RoundingBehavior.UP:
+                    ticker_delta = math.ceil(ticker_delta)
+                elif options.rounding_behavior == RoundingBehavior.DOWN:
+                    ticker_delta = math.floor(ticker_delta)
+                else:
+                    raise Exception("Rounding behavior required but not specified")
             delta_shares[mp_ticker] = ticker_delta
 
         for live_ticker in self.live_portfolio.all_tickers():
             if not self.model_portfolio.contains_ticker(live_ticker):
                 delta_shares[live_ticker] = -1 * self.live_portfolio.get_quantity(live_ticker)
         return delta_shares
+
+    def build_rebalanced_portfolio(self) -> Portfolio:
+        instructions = self.build_rebalance_instructions()
+        rebalanced_portfolio = Portfolio()
+        for ticker in self.live_portfolio.all_tickers():
+            adjusted_quantity = self.live_portfolio.get_quantity(ticker)
+            if ticker in instructions:
+                adjusted_quantity += instructions[ticker]
+                del instructions[ticker]
+            if adjusted_quantity != 0.0:
+                rebalanced_portfolio.add_position(Position(ticker, adjusted_quantity))
+        for ticker in instructions.keys():
+            rebalanced_portfolio.add_position(Position(ticker, instructions[ticker]))
+        return rebalanced_portfolio
 
 def build_price_lookup(implementation_records: dict) -> PriceLookup:
     prices = PriceLookup()
@@ -125,8 +156,8 @@ def build_live_portfolio(implementation_lookup: dict) -> Portfolio:
             portfolio.add_position(Position(ticker, ir.quantity))
     return portfolio
 
-def build_model_portfolio(total_value: float, summary_records: list, implementation_lookup: dict, prices: PriceLookup) -> Portfolio:
-    builder = ModelPortfolioBuilder(total_value, prices)
+def build_model_portfolio(options: RebalanceOptions, summary_records: list, implementation_lookup: dict, prices: PriceLookup) -> Portfolio:
+    builder = ModelPortfolioBuilder(options.desired_total_value, prices)
     for sr in summary_records:
         if sr.target_percentage > 0:
             builder.add_model_position(ModelPosition(sr.ticker, sr.target_percentage))
@@ -159,7 +190,7 @@ def validate_and_value_summary_records(summary_records: list, implementation_loo
         
         if abs(sr.actual_amount - total_impl_value) > VALUE_TOLERANCE:
             raise Exception(f"Value of implementation record {sr.ticker} (${total_impl_value}) does not match "
-            "value reflected in summary record (${sr.actual_amount})")
+            f"value reflected in summary record (${sr.actual_amount})")
     
     for impl_record in implementation_lookup.values():
         ticker = impl_record.ticker
@@ -174,9 +205,13 @@ def validate_and_value_summary_records(summary_records: list, implementation_loo
     return total_summary_values
 
 def capture_desired_portfolio_value(live_portfolio_value: float) -> float:
-    print(f"Your current portfolio value is ${live_portfolio_value:,.2f}.  Please input your desired total value. (Prefix '+' or '-' to instead specify an offset from the portfolio's current value.) Press return for no change:")
-    line = sys.stdin.readline()
-    if line == "\n":
+    print("")
+    print(f"Your current portfolio value is ${live_portfolio_value:,.2f}.\n"
+    "Please input your desired total value.\n"
+    "(Prefix '+' or '-' to specify an offset from the portfolio's current value.)\n"
+    "Press return to default to the portfolio's current value:")
+    line = sys.stdin.readline().rstrip()
+    if line == "":
         return live_portfolio_value
             
     match = re.search(r'^([\+\-])?\$?([\d.,]+)', line)
@@ -191,14 +226,37 @@ def capture_desired_portfolio_value(live_portfolio_value: float) -> float:
         return value_specified
 
 def capture_allow_fractional_shares() -> bool:
-    print(f"Allow fractional shares (yes/NO)?:")
+    print("Allow fractional shares (yes/NO)?: ", end = '')
     line = sys.stdin.readline()
-    return line[0].lower() == 'y'
+    answer = line[0].lower() == 'y'
+    return answer
+
+def capture_rounding_behavior() -> RoundingBehavior:
+    print("")
+    print("How should partial shares be rounded?\n"
+        "\t'+'\tAlways round up: Rebalancing may require additional cash\n"
+        "\t'-'\tAlways round down: Rebalancing may leave cash uninveseted\n"
+        "\t''\tRound to nearest: Rebalancing may either leave or require additional cash\n"
+
+        "Enter '+', '-', or press return for the default behavior: ", end='')
+    line = sys.stdin.readline().rstrip()
+    if line == "":
+        return RoundingBehavior.NEAREST
+    if line[0] == '+':
+        return RoundingBehavior.UP
+    elif line[0] == '-':
+        return RoundingBehavior.DOWN
+    raise Exception(f"Unrecognized rounding behavior '{line[0]}'")
+
+def capture_rebalance_options(live_portfolio_value: float) -> RebalanceOptions:
+    desired_portfolio_value = capture_desired_portfolio_value(live_portfolio_value)
+    allow_fractional_shares = capture_allow_fractional_shares()
+    rounding_behavior = RoundingBehavior.NEAREST if allow_fractional_shares else capture_rounding_behavior()
+    return RebalanceOptions(desired_portfolio_value, allow_fractional_shares, rounding_behavior)
 
 def capture_summary_records():
     print('''Expected Format (columns seperated by tabs):
-        <Asset Class Description (string)>,
-        <Implemention Ticker Description (string)>,
+        <Ticker Description (string)>,
         <Target Percentage From Model (% float)>,
         <$ Amount for Balanaced Portfolio ($ float)>
         <$ Amount for Actual Portfolio ($ float)")
@@ -212,8 +270,7 @@ def capture_summary_records():
         if line == "\n":
             break
         line = line.rstrip('\n')
-        regex = r"^(?P<asset_class>[^\t]*)\s*?\t"\
-            r"(?P<ticker>[^\t]*)\s*?\t"\
+        regex = r"^(?P<ticker>[^\t]*)\s*?\t"\
             r"(?P<target_percentage>[^\t]*)\s*?\t"\
             r"(?P<balanced_amount>[^\t]*)\s*?\t"\
             r"(?P<actual_amount>[^\t]*)"
@@ -221,9 +278,6 @@ def capture_summary_records():
         if match is None:
             raise Exception(f"Unable to read input line {line_count}: '{line}'")
         raw = match.groupdict()
-
-        # asset_class
-        asset_class = raw['asset_class'].strip()
 
         # ticker
         ticker = raw['ticker'].strip()
@@ -251,7 +305,7 @@ def capture_summary_records():
             actual_amount = aa_match.group(1).replace(',', '')
             actual_amount = float(actual_amount)
 
-        record = SummaryRecord(asset_class, ticker, target_percentage, balanced_amount, actual_amount)
+        record = SummaryRecord(ticker, target_percentage, balanced_amount, actual_amount)
         summary_records.append(record)
         line_count += 1
     return summary_records
@@ -318,24 +372,69 @@ def capture_implementation_records():
         else:
             raise Exception("Unexpected reading past line 3")
 
-def output_rebalance_instructions(rebalancer: PortfolioRebalancer, prices: PriceLookup):
+def output_rebalance_instructions(rebalancer: PortfolioRebalancer, prices: PriceLookup) -> None:
     instructions = rebalancer.build_rebalance_instructions()
     if len(instructions) == 0:
         print("No rebalance actions required.")
         return
+    
     print("Rebalance Instructions:")
     sorter = defaultdict(set)
     for ticker, quantity in instructions.items():
         if quantity != 0:
             sorter[prices.get_price(ticker) * quantity].add(ticker)
+
+    formatted_instructions = []
     for value in sorted(sorter.keys()):
         for ticker in sorter[value]:
             quantity = instructions[ticker]
-            quantity_str = f"{abs(quantity):,.2f}" if rebalancer.allow_fractional_shares else f"{abs(quantity):,}"
+            quantity_str = f"{abs(quantity):,.2f}" if rebalancer.options.allow_fractional_shares else f"{int(abs(quantity)):,}"
             value = quantity * prices.get_price(ticker)
             op = "BUY" if quantity > 0 else "SELL"
             sign = "" if quantity > 0 else "-"
-            print(f"\t{ticker}\t{op}\t{quantity_str}\tshares ({sign}${abs(value):,.2f})")
+            shares_str = "share" if abs(quantity) == 1.0 else "shares"
+            value_str = f"{abs(value):,.2f}"
+            formatted_instructions.append(RebalanceInstruction(ticker, op, quantity_str, shares_str, sign, value_str))
+
+    print("")
+    max_value_len = max([len(i.value_str) for i in formatted_instructions])
+    contains_sells = sum([len(i.sign) for i in formatted_instructions]) > 0
+    for i in formatted_instructions:
+        value_justified = i.value_str.rjust(max_value_len)
+        if contains_sells:
+            sign_justified = "-" if i.sign == '-' else " "
+        else:
+            sign_justified = ""
+        print(f"\t{i.ticker}\t{i.op}\t{i.quantity_str}\t{i.shares_str}\t({sign_justified}${value_justified})")
+    
+
+def output_rebalance_summary(rebalancer: PortfolioRebalancer, prices: PriceLookup) -> None:
+    print("")
+    output_rebalance_instructions(rebalancer, prices)
+    print("")
+
+    rebalanced_value = rebalancer.build_rebalanced_portfolio().calc_total_value(prices)
+    rebalanced_value_str = f"{rebalanced_value:,.2f}"
+    target_value = rebalancer.model_portfolio.calc_total_value(prices)
+    target_value_str = f"{target_value:,.2f}"
+    live_value = rebalancer.live_portfolio.calc_total_value(prices)
+    live_value_str = f"{live_value:,.2f}"
+    max_total_value_len = max(len(i) for i in [rebalanced_value_str, target_value_str, live_value_str])
+    rebalanced_value_str = rebalanced_value_str.rjust(max_total_value_len)
+    target_value_str = target_value_str.rjust(max_total_value_len)
+    live_value_str = live_value_str.rjust(max_total_value_len)
+    print(f"Value of rebalanced portfolio:\t${rebalanced_value_str}\n"
+        f"\tvs target value:\t${target_value_str}\n"
+        f"\tvs current value:\t${live_value_str}\n")
+
+    print("Options Applied:")
+    if rebalancer.options.allow_fractional_shares:
+        print("Fractional Shares: YES")
+    else:
+        print("\tFractional Shares: NO")
+        print(f"\tRounding Behavior: {rebalancer.options.rounding_behavior.name}")
+    print("")
+
 
 if __name__ == '__main__':
     print("\nWelcome to Portfolio Rebalancer\n")
@@ -346,25 +445,10 @@ if __name__ == '__main__':
     prices = build_price_lookup(implementation_lookup)
     live_portfolio = build_live_portfolio(implementation_lookup)
     
-    desired_total_value = capture_desired_portfolio_value(live_portfolio_value)
-    allow_fractional_shares = capture_allow_fractional_shares()
-    model_portfolio = build_model_portfolio(desired_total_value, summary_records, implementation_lookup, prices)
+    options = capture_rebalance_options(live_portfolio_value)
+    model_portfolio = build_model_portfolio(options, summary_records, implementation_lookup, prices)
 
-    rebalancer = PortfolioRebalancer(allow_fractional_shares)
+    rebalancer = PortfolioRebalancer(options)
     rebalancer.set_live_portfolio(live_portfolio)
     rebalancer.set_model_portfolio(model_portfolio)
-    output_rebalance_instructions(rebalancer, prices)
-
-
-
-
-    
-
-
-
-    
-
-        
-    
-
-
+    output_rebalance_summary(rebalancer, prices)
