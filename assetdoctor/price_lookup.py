44cef2578680afc76cdb4e5df7b5592f6d03e9cf
Eric Colton <ericcolton@gmail.com>
2020-08-26 16:21:52 -0400

    add price lookup

---
+++ assetdoctor/price_lookup.py
@@ -0,0 +1,19 @@
+'''
+Author: Eric Colton
+Copyright (c) 2020 Eric Colton
+'''
+
+class PriceLookup:
+    def __init__(self):
+        self.prices = {}
+
+    def add_price(self, ticker, price):
+        if ticker in self.prices:
+            raise Exception(f"Ticker '{ticker}' was already added")
+        self.prices[ticker] = price
+
+    def get_price(self, ticker):
+        if ticker not in self.prices:
+            raise Exception(f"Ticker '{ticker}' has associated price")
+        return self.prices[ticker]
+        
