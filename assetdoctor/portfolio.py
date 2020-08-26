620e545512157dc9f2834b57c8c3cfe037346252
Eric Colton <ericcolton@gmail.com>
2020-08-26 16:22:15 -0400

    add portfolio

---
+++ assetdoctor/portfolio.py
@@ -0,0 +1,36 @@
+'''
+Author: Eric Colton
+Copyright (c) 2020 Eric Colton
+'''
+
+from collections import namedtuple
+from .price_lookup import PriceLookup
+
+Position = namedtuple('Position', ['ticker', 'quantity'])
+
+class Portfolio:
+
+    def __init__(self):
+        self.positions = {}
+
+    def add_position(self, position: Position):
+        ticker = position.ticker
+        if position.ticker in self.positions:
+            raise Exception(f"Ticker '{ticker}' was already added")
+        self.positions[ticker] = position
+
+    def get_quantity(self, ticker: str):
+        if ticker in self.positions:
+            return self.positions[ticker].quantity
+        else:
+            return 0
+
+    def contains_ticker(self, ticker: str):
+        return ticker in self.positions
+
+    def all_tickers(self) -> list:
+        return self.positions.keys()
+
+    def calc_total_value(self, prices: PriceLookup):
+        return sum([prices.get_price(p.ticker) * p.quantity for p in self.positions.values()])
+
